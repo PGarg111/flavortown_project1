a894467d6b69818217eb9243d0edb385be804147
Prisha <p.garg1606@gmail.com>
2026-01-09 16:18:39 -0500

    added travel part to my website, still working on it

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function toggleTheme() {
 function showPage(pageId) {
 
     const pages = document.querySelectorAll(".page");
+
     pages.forEach(page => {
         if (page.id === pageId) {
             page.classList.remove("hidden");
